5bbd88b2ca2cd6a4ff45f10382d87f3d5536d8d6
Arnaud Porterie <arnaud.porterie@docker.com>
2015-11-08 05:14:19 -0800

    Add tests for blobstore and client
    Signed-off-by: Arnaud Porterie <arnaud.porterie@docker.com>

--- src/cmd/vossibility-collector/blobstore_test.go
+++ src/cmd/vossibility-collector/blobstore_test.go
@@ -24,36 +24,77 @@ func (t *testIndexer) Index(destination string, blob *Blob) error {
 	return nil
 }
 
+func (t *testIndexer) Len() int {
+	return len(*t)
+}
+
 func (t *testIndexer) Reset() {
 	*t = []indexCall{}
 }
 
-func TestSimpleBlobStoreLive(t *testing.T) {
+func simpleBlobStoreSetup() (blobStore, *testIndexer) {
 	s := simpleBlobStore{}
 	indexer := testIndexer{}
 	s.indexer = &indexer
+	return &s, &indexer
+}
 
+func TestSimpleBlobStoreLiveWithoutSnapshot(t *testing.T) {
+	s, indexer := simpleBlobStoreSetup()
 	b := NewBlob("event", "id")
 	if err := s.Store(StoreLiveEvent, &testRepository, b); err != nil {
 		t.Fatalf("failed to store blob: %v", err)
 	}
-	if len(indexer) != 1 {
-		t.Fatalf("indexer was called %d times, expected once", len(indexer))
+	if indexer.Len() != 1 {
+		t.Fatalf("indexer was called %d times, expected once", indexer.Len())
 	}
 }
 
-func TestSimpleBlobStoreLiveWithSnapshot(t *testing.T) {
-	s := simpleBlobStore{}
-	indexer := testIndexer{}
-	s.indexer = &indexer
+func TestSimpleBlobStoreCascading(t *testing.T) {
+	s, indexer := simpleBlobStoreSetup()
 
+	// Create a snapshottable blob.
 	b := NewBlob("event", "id")
 	b.Push(MetadataSnapshotID, "snapshot_id")
 	b.Push(MetadataSnapshotField, "snapshot_field")
+
+	// Verify that storing to StoreSnapshot doesn't cascade to any other store.
+	if err := s.Store(StoreSnapshot, &testRepository, b); err != nil {
+		t.Fatalf("failed to store blob: %v", err)
+	}
+	if indexer.Len() != 1 {
+		t.Fatalf("indexer was called %d times, expected once", indexer.Len())
+	}
+	destSnapshot := (*indexer)[0].Destination
+
+	// Verify that storing to StoreCurrentState cascades to StoreSnapshot using
+	// the same destination than a direct call.
+	indexer.Reset()
+	if err := s.Store(StoreCurrentState, &testRepository, b); err != nil {
+		t.Fatalf("failed to store blob: %v", err)
+	}
+	if indexer.Len() != 2 {
+		t.Fatalf("indexer was called %d times, expected once", indexer.Len())
+	}
+	destCurrent := (*indexer)[0].Destination
+	if cascading := (*indexer)[1].Destination; cascading != destSnapshot {
+		t.Fatalf("cascading current snapshot to %q, expected %q", cascading, destSnapshot)
+	}
+
+	// Verify that storing to StoreLiveEvent cascades to StoreCurrentState using
+	// the same destination than a direct call, and to StoreSnapshot using the
+	// same destination than a direct call.
+	indexer.Reset()
 	if err := s.Store(StoreLiveEvent, &testRepository, b); err != nil {
 		t.Fatalf("failed to store blob: %v", err)
 	}
-	if len(indexer) != 3 {
-		t.Fatalf("indexer was called %d times, expected 3", len(indexer))
+	if indexer.Len() != 3 {
+		t.Fatalf("indexer was called %d times, expected once", indexer.Len())
+	}
+	if cascading := (*indexer)[1].Destination; cascading != destCurrent {
+		t.Fatalf("cascading live event to %q, expected %q", cascading, destCurrent)
+	}
+	if cascading := (*indexer)[2].Destination; cascading != destSnapshot {
+		t.Fatalf("cascading live event to %q, expected %q", cascading, destSnapshot)
 	}
 }
---
+++ src/cmd/vossibility-collector/client_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	gh "github.com/google/go-github/github"
+)
+
+func simulateAPIRequest(t *testing.T, c *gh.Client) []*http.Request {
+	var requests []*http.Request
+	mux := http.NewServeMux()
+	mux.HandleFunc("/rate_limit", func(w http.ResponseWriter, req *http.Request) {
+		requests = append(requests, req)
+		w.Write([]byte("{}"))
+	})
+
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	c.BaseURL, _ = url.Parse(srv.URL)
+	if _, _, err := c.RateLimits(); err != nil {
+		t.Fatalf("failed to retrieve rate limits; %v", err)
+	}
+	return requests
+}
+
+func TestClientWithoutToken(t *testing.T) {
+	c := NewClient(&Config{})
+	requests := simulateAPIRequest(t, c)
+	if len(requests) != 1 {
+		t.Fatalf("unexpected number of requests %d, expected 1", len(requests))
+	}
+	if _, ok := requests[0].Header["Authorization"]; ok {
+		t.Fatal("unexpected Authorization header in request")
+	}
+}
+
+func TestClientWithToken(t *testing.T) {
+	c := NewClient(&Config{GitHubAPIToken: "t0k3n"})
+	requests := simulateAPIRequest(t, c)
+	if len(requests) != 1 {
+		t.Fatalf("unexpected number of requests %d, expected 1", len(requests))
+	}
+	if v, ok := requests[0].Header["Authorization"]; !ok || len(v) != 1 {
+		t.Fatal("expected single-value Authorization header in request")
+	} else if expected := "Bearer t0k3n"; v[0] != expected {
+		t.Fatalf("got Authorization token %q, expected %q", v[0], expected)
+	}
+}
